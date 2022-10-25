c70fbe4ebf273deb4cffbff3629c9adb92126418
juandavidGF <davad701@gmail.com>
2022-10-25 15:07:32 -0500

    add delete and getKeys, hashTable

--- hashTable.js
+++ hashTable.js
@@ -31,6 +31,46 @@ class HashTable {
 		}
 		return undefined;
 	}
+
+	delete(key) {
+		let address = this.HashMethod(key);
+		let currentBucket = this.data[address];
+		if(currentBucket) {
+			for(let i = 0; i < currentBucket.length; i++) {
+				if(currentBucket[i][0] === key) {
+					if(this.data[address].length === 1) {
+						delete this.data[address]
+						return currentBucket[i];
+					}
+					// delete this.data[address][i];
+					this.shiftIndex(address, i, currentBucket.length)
+					return currentBucket[i];
+				}
+			}
+		}
+		return undefined;
+	}
+
+	shiftIndex(address, i, lengthBucket) {
+		for(let j = i; j < lengthBucket - 1; j++) {
+			this.data[address][j] = this.data[address][j+1];
+		}
+		delete this.data[address][lengthBucket - 1];
+	}
+
+	getKeys() {
+		const keys = []
+		for(let i = 0; i < this.data.length; i++) {
+			if(this.data[i]) {
+				for(let j = 0; j < this.data[i].length; j++) {
+					if(this.data[i][j]) {
+						keys.push(this.data[i][j][0]);
+					}
+				}
+			}
+		}
+		return keys;
+	}
 }
 
 const myHashTable = new HashTable(50);
