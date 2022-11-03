32132a898adf9412611fd4330523acb6f6c9fc47
juandavidGF <davad701@gmail.com>
2022-11-03 10:02:34 -0500

    stack

---
+++ stack.js
@@ -0,0 +1,45 @@
+
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class Stack {
+	constructor() {
+		this.top = null;
+		this.bottom = null;
+		this.length = 0;
+	}
+
+	push(value) {
+		const newNode = new Node(value);
+
+		if(this.length === 0) {
+			this.top = newNode;
+			this.bottom = newNode;
+		} else {
+			const holdingNode = this.top;
+			this.top = newNode;
+			this.top.next = holdingNode;
+		}
+		this.length++;
+
+		return this;
+	}
+
+	peek() {
+		return this.top;
+	}
+
+	pop() {
+		const holdingNode = this.top;
+		this.top = this.top.next;
+		this.length--;
+
+		return this;
+	}
+}
+
+const myStack = new Stack;
